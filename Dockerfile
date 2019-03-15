0c79337c7f9b046779176acacb07e354d803d9a2
Valentin Orban <orban.valentin@gmail.com>
2019-03-15 08:32:35 +0100

    corrected python bugs

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,7 @@ FROM python:3.7-alpine
 
 WORKDIR /usr/src/app
 
-COPY papi.py ./
-COPY requirements.txt ./
+COPY src ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 CMD python papi.py
--- src/api_fetcher.py
+++ src/api_fetcher.py
@@ -4,11 +4,11 @@ _URL = "http://192.168.0.16:8545/"
 _HEADERS = {'content-type': 'application/json'}
 
 '''Sends request to RPC API of the service specified by url'''
-def send_request(url=_URL, method, params=[]):
+def send_request(method, url=_URL, params=[]):
 	payload = {
 		"method":method,
 		"params":params,
 		"id":1,
 		"jsonrpc":"2.0"
 	}
-	return requests.post(url, data=json.dumps(payload), headers=headers).json()
+	return requests.post(url, data=json.dumps(payload), headers=_HEADERS).json()
