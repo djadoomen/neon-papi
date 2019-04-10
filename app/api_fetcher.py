54f68fc04d2dcd7622db8437672b6d6d3b71d76e
Valentin Orban <orban.valentin@gmail.com>
2019-04-10 19:07:34 +0200

    adding peers working

--- app/api_fetcher.py
+++ app/api_fetcher.py
@@ -1,9 +1,9 @@
-import json, requests
+import json, requests, socket
 
 _URL = "http://node:8545/"
 _HEADERS = {'content-type': 'application/json'}
 
-def send_request(method, url=None, params=[]):
+def send_request(method, params=[], url=None):
 	payload = {
 		'method':method,
 		'params':params,
@@ -14,4 +14,45 @@ def send_request(method, url=None, params=[]):
 		_URL if url is None else url,
 		data=json.dumps(payload),
 		headers=_HEADERS
-	).json()
+	)
+
+def register_ips():
+	ips = []
+	for i in range(20):
+		ip = socket.gethostbyname('tasks.node')
+		if ip not in ips:
+			ips.append(ip)
+	return ips
+
+def register_nodes(ips):
+	nodes = list()
+	for ip in ips:
+		node = send_request('admin_nodeInfo', [], 'http://' + ip + ':8545/').json()['result']['enode']
+		if node not in nodes: nodes.append(node)
+	return nodes
+
+def setup_peers():
+	ips = register_ips()
+	nodes = register_nodes(ips)
+	for i in range(len(nodes)):
+		success = send_request('admin_addPeer', [nodes[i].replace('127.0.0.1', ips[i])])
+		print(success.json())
+	return nodes
+# {
+# "jsonrpc":"2.0",
+# "id":1,
+# "result":{
+# 	"id":"c3af701a968e5e607cb5c7665d0f2decb4eeeb433aaaea012930ba0b469d8500",
+# 	"name":"Geth/v1.8.23-stable-c9427004/linux-amd64/go1.11.5",
+# 	"enode":"enode://c800980f86316f5eed90b6dc841b27c93d287b74550b7f671aa2dd66082908738cf59fd94e10ab9c6836d85b031ea7e200886c667cdb118d1f0b53e4f1e05bf1@127.0.0.1:30303",
+# 	"enr":"0xf896b840fb47b4050aa18b5d39ac09b71a83e413c2eae254fe36e911c153f973efeda4225e96796574817f0bec5c9a4aa1e532d77f9af5ccde74a8861aa270d94621a74d0183636170c6c5836574683f826964827634826970847f00000189736563703235366b31a103c800980f86316f5eed90b6dc841b27c93d287b74550b7f671aa2dd66082908738374637082765f8375647082765f",
+# 	"ip":"127.0.0.1",
+# 	"ports":{
+# 		"discovery":30303,
+# 		"listener":30303
+# 	},
+# 	"listenAddr":"[::]:30303",
+# 	"protocols":{
+# 		"eth":{"network":215037,"difficulty":131072,"genesis":"0xae9394e18d5e7e976599209ef7ed31c8e9ed7712bb177db7b1ce0ced27ceb1fd","config":{"chainId":215037,"homesteadBlock":0,"eip150Hash":"0x0000000000000000000000000000000000000000000000000000000000000000","eip155Block":0,"eip158Block":0},"head":"0xae9394e18d5e7e976599209ef7ed31c8e9ed7712bb177db7b1ce0ced27ceb1fd"}}
+# 	}
+# }
--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
 from flask import Flask
 
-from api_fetcher import send_request
+import api_fetcher as api
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return send_request('admin_peers')
+def index():
+    return api.send_request('admin_peers').text
+
+@app.route("/balance")
+def balance():
+	return api.send_request('eth_getBalance', ["0xb936b619831b77910fb38eed3f80791de6ca21b7", "latest"]).text
+
+@app.route("/peers")
+def peers():
+	return api.send_request('net_peerCount').text
+
+@app.route("/info")
+def info():
+	return api.send_request('admin_nodeInfo').text
+
+@app.route("/setup_peers")
+def setup_peers():
+	return '\n'.join(api.setup_peers())
+
+@app.route("/ip")
+def ip():
+	return ' '.join(api.register_ips())
 
 
 # if __name__ == "__main__":
